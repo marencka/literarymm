5e6796afc734b0e9ae378cb7b23d703acfc12546
marencka <57925696+marencka@users.noreply.github.com>
2022-05-02 13:47:23 -0400

    Fixed PDPR button and instruction for non PD users

--- base/static/js/dashboard.js
+++ base/static/js/dashboard.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     let day = new Date().getDay();
+    console.log(day);
     // 0 - sunday, 1 - monday, 2 - tuesday, 3 - wednesday, 4 - thursday, 5 - friday, 6 - saturday
     let isFriday = false;
     if (day != 5) {
@@ -14,11 +15,13 @@ window.onload = function () {
         }
 
         let pdpr_button = document.getElementById('pdpr');
+        let pdpr_class = document.getElementById('pdpr_class');
 
         if ((typeof (pdpr_button) == 'undefined') || (pdpr_button == null)) {
             return;
         }
         pdpr_button.disabled = 'true';
+        pdpr_class.style.display = 'none';
         pdpr_button.style.backgroundColor = "rgba(82, 114, 110, 0.8)";
     }
 
